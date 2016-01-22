193cc1d11eaebca94a6799ae28660c2057c3508b
Fernando Villalobos <fernando.visa@gmail.com>
2016-01-21 20:45:08 -0600

    Enable JSON/XML API to fetch products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+      format.xml { render xml: @products }
+    end
   end
 
   def show
